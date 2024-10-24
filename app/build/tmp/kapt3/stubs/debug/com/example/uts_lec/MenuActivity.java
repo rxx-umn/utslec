7571b13a1ee47eb205948fbfd052590219a3175a
Izdihar Dhawy Tasdid Tasdid <129589195+Oiz3e@users.noreply.github.com>
2024-10-24 18:25:15 +0700

    merge mamat x main

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/uts_lec/MenuActivity.java
@@ -0,0 +1,145 @@
+package com.example.uts_lec;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000t\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000b\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0010\u0007\n\u0002\b\u0004\n\u0002\u0010\u0011\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\u0015\n\u0002\b\u000b\u0018\u00002\u00020\u00012\u00020\u0002:\u00016B\u0005\u00a2\u0006\u0002\u0010\u0003J\b\u0010\u0011\u001a\u00020\u0012H\u0002J\b\u0010\u0013\u001a\u00020\u0012H\u0002J\"\u0010\u0014\u001a\u00020\u00122\u0006\u0010\u0015\u001a\u00020\u00052\u0006\u0010\u0016\u001a\u00020\u00052\b\u0010\u0017\u001a\u0004\u0018\u00010\u0018H\u0014J\u0012\u0010\u0019\u001a\u00020\u00122\b\u0010\u001a\u001a\u0004\u0018\u00010\u001bH\u0014J\u0010\u0010\u001c\u001a\u00020\u001d2\u0006\u0010\u001e\u001a\u00020\u001fH\u0016J*\u0010 \u001a\u00020\u001d2\b\u0010!\u001a\u0004\u0018\u00010\u001f2\u0006\u0010\"\u001a\u00020\u001f2\u0006\u0010#\u001a\u00020$2\u0006\u0010%\u001a\u00020$H\u0016J\u0010\u0010&\u001a\u00020\u00122\u0006\u0010\u001e\u001a\u00020\u001fH\u0016J+\u0010\'\u001a\u00020\u00122\u0006\u0010\u0015\u001a\u00020\u00052\f\u0010(\u001a\b\u0012\u0004\u0012\u00020*0)2\u0006\u0010+\u001a\u00020,H\u0016\u00a2\u0006\u0002\u0010-J*\u0010.\u001a\u00020\u001d2\b\u0010!\u001a\u0004\u0018\u00010\u001f2\u0006\u0010\"\u001a\u00020\u001f2\u0006\u0010/\u001a\u00020$2\u0006\u00100\u001a\u00020$H\u0016J\u0010\u00101\u001a\u00020\u00122\u0006\u0010\u001e\u001a\u00020\u001fH\u0016J\u0010\u00102\u001a\u00020\u001d2\u0006\u0010\u001e\u001a\u00020\u001fH\u0016J\u0010\u00103\u001a\u00020\u001d2\u0006\u00104\u001a\u00020\u001fH\u0016J\b\u00105\u001a\u00020\u0012H\u0002R\u000e\u0010\u0004\u001a\u00020\u0005X\u0082D\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0006\u001a\u00020\u0005X\u0082D\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\bX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\t\u001a\u00020\nX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u000b\u001a\u00020\fX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\r\u001a\u00020\u000eX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u000f\u001a\u00020\u0010X\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u00067"}, d2 = {"Lcom/example/uts_lec/MenuActivity;", "Landroidx/activity/ComponentActivity;", "Landroid/view/GestureDetector$OnGestureListener;", "()V", "REQUEST_CAMERA_PERMISSION", "", "REQUEST_IMAGE_CAPTURE", "databaseRef", "Lcom/google/firebase/database/DatabaseReference;", "gestureDetector", "Landroid/view/GestureDetector;", "photoFile", "Ljava/io/File;", "postCaptionView", "Landroid/widget/TextView;", "postImageView", "Landroid/widget/ImageView;", "checkCameraPermission", "", "loadPostsFromFirebase", "onActivityResult", "requestCode", "resultCode", "data", "Landroid/content/Intent;", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "onDown", "", "e", "Landroid/view/MotionEvent;", "onFling", "e1", "e2", "velocityX", "", "velocityY", "onLongPress", "onRequestPermissionsResult", "permissions", "", "", "grantResults", "", "(I[Ljava/lang/String;[I)V", "onScroll", "distanceX", "distanceY", "onShowPress", "onSingleTapUp", "onTouchEvent", "event", "openCamera", "Post", "app_debug"})
+public final class MenuActivity extends androidx.activity.ComponentActivity implements android.view.GestureDetector.OnGestureListener {
+    private android.view.GestureDetector gestureDetector;
+    private final int REQUEST_CAMERA_PERMISSION = 100;
+    private final int REQUEST_IMAGE_CAPTURE = 101;
+    private java.io.File photoFile;
+    private android.widget.ImageView postImageView;
+    private android.widget.TextView postCaptionView;
+    private com.google.firebase.database.DatabaseReference databaseRef;
+    
+    public MenuActivity() {
+        super(0);
+    }
+    
+    @java.lang.Override
+    protected void onCreate(@org.jetbrains.annotations.Nullable
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    private final void loadPostsFromFirebase() {
+    }
+    
+    @java.lang.Override
+    public boolean onTouchEvent(@org.jetbrains.annotations.NotNull
+    android.view.MotionEvent event) {
+        return false;
+    }
+    
+    @java.lang.Override
+    public boolean onFling(@org.jetbrains.annotations.Nullable
+    android.view.MotionEvent e1, @org.jetbrains.annotations.NotNull
+    android.view.MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+    
+    private final void checkCameraPermission() {
+    }
+    
+    @java.lang.Override
+    public void onRequestPermissionsResult(int requestCode, @org.jetbrains.annotations.NotNull
+    java.lang.String[] permissions, @org.jetbrains.annotations.NotNull
+    int[] grantResults) {
+    }
+    
+    private final void openCamera() {
+    }
+    
+    @java.lang.Override
+    protected void onActivityResult(int requestCode, int resultCode, @org.jetbrains.annotations.Nullable
+    android.content.Intent data) {
+    }
+    
+    @java.lang.Override
+    public boolean onDown(@org.jetbrains.annotations.NotNull
+    android.view.MotionEvent e) {
+        return false;
+    }
+    
+    @java.lang.Override
+    public void onShowPress(@org.jetbrains.annotations.NotNull
+    android.view.MotionEvent e) {
+    }
+    
+    @java.lang.Override
+    public boolean onSingleTapUp(@org.jetbrains.annotations.NotNull
+    android.view.MotionEvent e) {
+        return false;
+    }
+    
+    @java.lang.Override
+    public boolean onScroll(@org.jetbrains.annotations.Nullable
+    android.view.MotionEvent e1, @org.jetbrains.annotations.NotNull
+    android.view.MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+    
+    @java.lang.Override
+    public void onLongPress(@org.jetbrains.annotations.NotNull
+    android.view.MotionEvent e) {
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\t\n\u0002\u0010\u000b\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\b\u0086\b\u0018\u00002\u00020\u0001B\u0019\u0012\b\b\u0002\u0010\u0002\u001a\u00020\u0003\u0012\b\b\u0002\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0005J\t\u0010\t\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\n\u001a\u00020\u0003H\u00c6\u0003J\u001d\u0010\u000b\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u0003H\u00c6\u0001J\u0013\u0010\f\u001a\u00020\r2\b\u0010\u000e\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010\u000f\u001a\u00020\u0010H\u00d6\u0001J\t\u0010\u0011\u001a\u00020\u0003H\u00d6\u0001R\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0006\u0010\u0007R\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\b\u0010\u0007\u00a8\u0006\u0012"}, d2 = {"Lcom/example/uts_lec/MenuActivity$Post;", "", "imageUrl", "", "caption", "(Ljava/lang/String;Ljava/lang/String;)V", "getCaption", "()Ljava/lang/String;", "getImageUrl", "component1", "component2", "copy", "equals", "", "other", "hashCode", "", "toString", "app_debug"})
+    public static final class Post {
+        @org.jetbrains.annotations.NotNull
+        private final java.lang.String imageUrl = null;
+        @org.jetbrains.annotations.NotNull
+        private final java.lang.String caption = null;
+        
+        public Post(@org.jetbrains.annotations.NotNull
+        java.lang.String imageUrl, @org.jetbrains.annotations.NotNull
+        java.lang.String caption) {
+            super();
+        }
+        
+        @org.jetbrains.annotations.NotNull
+        public final java.lang.String getImageUrl() {
+            return null;
+        }
+        
+        @org.jetbrains.annotations.NotNull
+        public final java.lang.String getCaption() {
+            return null;
+        }
+        
+        public Post() {
+            super();
+        }
+        
+        @org.jetbrains.annotations.NotNull
+        public final java.lang.String component1() {
+            return null;
+        }
+        
+        @org.jetbrains.annotations.NotNull
+        public final java.lang.String component2() {
+            return null;
+        }
+        
+        @org.jetbrains.annotations.NotNull
+        public final com.example.uts_lec.MenuActivity.Post copy(@org.jetbrains.annotations.NotNull
+        java.lang.String imageUrl, @org.jetbrains.annotations.NotNull
+        java.lang.String caption) {
+            return null;
+        }
+        
+        @java.lang.Override
+        public boolean equals(@org.jetbrains.annotations.Nullable
+        java.lang.Object other) {
+            return false;
+        }
+        
+        @java.lang.Override
+        public int hashCode() {
+            return 0;
+        }
+        
+        @java.lang.Override
+        @org.jetbrains.annotations.NotNull
+        public java.lang.String toString() {
+            return null;
+        }
+    }
+}
